8666fd11d8c82a223e53571d4159655602b6b426
Serkan Can Caglayan <49690023+serkancancaglayan@users.noreply.github.com>
2022-11-13 10:16:04 +0300

    Update HREstimator.py

--- HREstimator.py
+++ HREstimator.py
@@ -35,67 +35,6 @@ def captureFrames(source, duration):
     return frameBuffer, samplingRate
 
 
-def estimateHR(frameBuffer, samplingRate, plot=1):
-    meansROI1 = []
-    meansROI2 = []
-    meansROI3 = []
-
-    for frame in frameBuffer:
-        try:
-            startX, startY, endX, endY = detectFace(frame)
-            facialLandmarks = detectLandmarks(frame, startX, startY, endX, endY)
-            roiRightCheek, roiLeftCheek, roiForeHead = extractROIs(frame, facialLandmarks, startY)
-
-            mROI1 = np.mean(extractGreenChannel(roiRightCheek))
-            mROI2 = np.mean(extractGreenChannel(roiLeftCheek))
-            mROI3 = np.mean(extractGreenChannel(roiForeHead))
-
-            meansROI1.append(mROI1)
-            meansROI2.append(mROI2)
-            meansROI3.append(mROI3)
-        except:
-            meansROI1.append(0)
-            meansROI2.append(0)
-            meansROI3.append(0)
-        cv.imshow('Calculating HR...', frame)
-        if cv.waitKey(1) == 27:
-            break
-
-    meansROI1 = np.array(meansROI1)
-    meansROI2 = np.array(meansROI2)
-    meansROI3 = np.array(meansROI3)
-
-    normalizedROI1 = zeroCenterNormalization(meansROI1)
-    normalizedROI2 = zeroCenterNormalization(meansROI2)
-    normalizedROI3 = zeroCenterNormalization(meansROI3)
-
-    denoisedROI1 = denoiseWavelet(normalizedROI1)
-    denoisedROI2 = denoiseWavelet(normalizedROI2)
-    denoisedROI3 = denoiseWavelet(normalizedROI3)
-
-    bandPassedROI1 = bandPassFilter(denoisedROI1, samplingRate, 1, 2.75, 3)
-    bandPassedROI2 = bandPassFilter(denoisedROI2, samplingRate, 1, 2.75, 3)
-    bandPassedROI3 = bandPassFilter(denoisedROI3, samplingRate, 1, 2.75, 3)
-
-    final_psd = getPSD(bandPassedROI1) * getPSD(bandPassedROI2) * getPSD(bandPassedROI3)
-    final_signal = np.fft.ifft(final_psd)
-
-    d = {'ppg_signal': final_signal}
-    savemat('ppg_signal.mat', d)
-    windowedSignal = hammingWindow(final_signal)
-    HRRange, powerSpect = getPowerSpectrum(windowedSignal, samplingRate, 1, 2.75)
-    HR = int(HRRange[np.argmax(powerSpect)])
-
-    if plot:
-        plt.figure(figsize=(15, 7))
-        plt.plot(final_signal), plt.title('rPPG Signal')
-        plt.figure()
-        plt.plot(HRRange, powerSpect), plt.axvspan(HR - 2, HR + 4, color='r', alpha=0.2), plt.title('Power Spectrum')
-        plt.show()
-
-    return HR, final_signal
-
-
 def estimateHRSlices(frameBuffer, samplingRate, plot=1):
     allSliceMeans = list()
     for frame in frameBuffer:
