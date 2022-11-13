2efb917df9c22ed9f1b3c2ed0c329dae82436704
Serkan Can Caglayan <49690023+serkancancaglayan@users.noreply.github.com>
2022-11-13 10:16:55 +0300

    Update FaceUtilities.py

--- FaceUtilities.py
+++ FaceUtilities.py
@@ -1,12 +1,10 @@
 import cv2 as cv
-import dlib as dl
 from imutils import face_utils
 from SignalProcessing import *
 from matplotlib import pyplot as plt
 
 
 faceDetectorModelConfigs = ['./cascades/deploy.prototxt.txt', './cascades/res10_300x300_ssd_iter_140000.caffemodel']
-shapePredictorModelConfig = './cascades/shape_predictor_68_face_landmarks.dat'
 
 faceDetectorModel = cv.dnn.readNetFromCaffe(faceDetectorModelConfigs[0], faceDetectorModelConfigs[1])
 availableGPU = cv.cuda.getCudaEnabledDeviceCount()
@@ -14,7 +12,6 @@ if availableGPU > 0:
     faceDetectorModel.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
     faceDetectorModel.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
 
-shapePredictorModel = dl.shape_predictor(shapePredictorModelConfig)
 
 
 def detectFace(frame, drawRect=1):
@@ -38,28 +35,6 @@ def detectFace(frame, drawRect=1):
         cv.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 255), 1)
     return startX, startY, endX, endY
 
-
-def detectLandmarks(frame, startX, startY, endX, endY):
-    faceRect = dl.rectangle(startX, startY, endX, endY)
-    facialLandmarks = shapePredictorModel(frame, faceRect)
-    facialLandmarks = face_utils.shape_to_np(facialLandmarks)
-    return facialLandmarks
-
-
-def extractROIs(frame, facialLandmarks, startY, drawRect=1):
-    roiRightCheek = frame[facialLandmarks[29][1]:facialLandmarks[33][1], facialLandmarks[54][0]:facialLandmarks[12][0]]
-    roiLeftCheek = frame[facialLandmarks[29][1]:facialLandmarks[33][1], facialLandmarks[4][0]:facialLandmarks[48][0]]
-    roiForeHead = frame[startY + 20:facialLandmarks[19][1], facialLandmarks[18][0]:facialLandmarks[25][0]]
-
-    if drawRect:
-        cv.rectangle(frame, (facialLandmarks[54][0], facialLandmarks[29][1]),
-                     (facialLandmarks[12][0], facialLandmarks[33][1]), (255, 255, 255), 1)
-        cv.rectangle(frame, (facialLandmarks[4][0], facialLandmarks[29][1]),
-                     (facialLandmarks[48][0], facialLandmarks[33][1]), (255, 255, 255), 1)
-        cv.rectangle(frame, (facialLandmarks[18][0], startY + 20), (facialLandmarks[25][0], facialLandmarks[19][1]),
-                     (255, 255, 255), 1)
-
-    return roiRightCheek, roiLeftCheek, roiForeHead
 
 
 def sliceFace(frame, startX, startY, endX, endY, marginScale=10):
